f0c17b94e7a52a253086ed7234129bc8bc6cb093
Joseph Le Grice <joe.legrice@gmail.com>
2025-06-19 12:40:54 +0100

    aim ahead wip

--- Assets/Scripts/Combat/Targeting/ShipHardpointGimbalingSystem.cs
+++ Assets/Scripts/Combat/Targeting/ShipHardpointGimbalingSystem.cs
@@ -20,8 +20,10 @@ public partial struct ShipHardpointGimbalingSystem : ISystem
         ComponentLookup<LocalToWorld> localToWorldLookup = SystemAPI.GetComponentLookup<LocalToWorld>();
         ComponentLookup<LocalTransform> localTransformLookup = SystemAPI.GetComponentLookup<LocalTransform>();
         ComponentLookup<ShipHardpointInstance> hardpointInstanceLookup = SystemAPI.GetComponentLookup<ShipHardpointInstance>();
+        ComponentLookup<PhysicsVelocity> physicsVelocityLookup = SystemAPI.GetComponentLookup<PhysicsVelocity>();
+        ComponentLookup<ProjectileSourceConfiguration> projectileSourceConfigLookup = SystemAPI.GetComponentLookup<ProjectileSourceConfiguration>();
         
-        foreach (var (shipHardpointBuffer, targetBuffer, velocity) in SystemAPI.Query<DynamicBuffer<ShipHardpointReference>, DynamicBuffer<DetectedTarget>, RefRO<PhysicsVelocity>>())
+        foreach (var (shipHardpointBuffer, targetBuffer, self) in SystemAPI.Query<DynamicBuffer<ShipHardpointReference>, DynamicBuffer<DetectedTarget>>().WithEntityAccess())
         {
             if (GetSelectedTarget(ref state, targetBuffer, out DetectedTarget selectedTarget))
             {
@@ -36,7 +38,27 @@ public partial struct ShipHardpointGimbalingSystem : ISystem
                     float3 worldDir = l2wTarget.Position - l2wHardpoint.Position;
 
                     float3 aimAhead = new float3();
-                    aimAhead += velocity.ValueRO.Linear * SystemAPI.Time.DeltaTime;
+                    if (physicsVelocityLookup.HasComponent(self) && projectileSourceConfigLookup.HasComponent(hardpoint.ValueRO.WeaponInstanceEntity))
+                    {
+                        // todo: work on aim ahead
+                        var config = projectileSourceConfigLookup[hardpoint.ValueRO.WeaponInstanceEntity];
+                        
+                        PhysicsVelocity velocitySelf = physicsVelocityLookup[self];
+                        float likeness = math.dot(math.normalize(worldDir), velocitySelf.Linear);
+
+                        float distanceToTarget = math.distance(l2wTarget.Position, localToWorldLookup[config.FireNode].Position);
+                        float timeToTarget = distanceToTarget / (config.ProjectileSpeed + likeness);
+
+                        float remove = math.sqrt(math.lengthsq(velocitySelf.Linear) - math.square(likeness));
+                        float3 dir = math.cross(math.normalize(worldDir), math.cross(math.normalize(worldDir), math.normalizesafe(velocitySelf.Linear)));
+                        aimAhead += remove * dir * timeToTarget;
+
+                        // if (physicsVelocityLookup.HasComponent(selectedTarget.TargetableEntity))
+                        // {
+                        //     PhysicsVelocity velocityTarget = physicsVelocityLookup[selectedTarget.TargetableEntity];
+                        //     aimAhead += math.normalizesafe(velocityTarget.Linear) * timeToTarget;
+                        // }
+                    }
                     
                     worldDir += aimAhead;
                     
@@ -44,7 +66,7 @@ public partial struct ShipHardpointGimbalingSystem : ISystem
                     float angle = math.degrees(math.acos(math.dot(l2wHardpoint.Forward, worldDirNormalised)));
 
                     float3 targetLocalForward = new float3(0.0f, 0.0f, 1.0f);
-                    if (angle < 45.0f)
+                    // if (angle < 45.0f)
                     {
                         targetLocalForward = l2wHardpoint.Value.InverseTransformDirection(worldDirNormalised);
                     }
@@ -79,7 +101,8 @@ public partial struct ShipHardpointGimbalingSystem : ISystem
                     quaternion fromToRotation = PhysicsHelpers.GetFromToRotation(ltGimbal.Forward(), hardpoint.TargetLocalForward);
                     // fromToRotation.value.w = math.min(fromToRotation.value.w, adjustSpeed * SystemAPI.Time.DeltaTime);
                     float3 newForward = math.rotate(fromToRotation, ltGimbal.Forward());
-                    ltGimbal.Rotation = quaternion.LookRotation(newForward, new float3(0, 1, 0));
+                    // ltGimbal.Rotation = quaternion.LookRotation(newForward, new float3(0, 1, 0));
+                    ltGimbal.Rotation = quaternion.LookRotation(hardpoint.TargetLocalForward, new float3(0, 1, 0));
                     
                     localTransformLookup[hardpointGimbal.GimbalEntity] = ltGimbal;
                 }
